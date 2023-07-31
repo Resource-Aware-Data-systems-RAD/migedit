8b12f313b5a3cd9da6d52a9f063bcb9a3e9ec4d8
Ties Robroek <titr@itu.dk>
2023-07-31 14:11:30 +0200

    Version 3.0

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # migedit
-MIG Editor python CLI tool and bindings for MIG device management
+MIG Editor python CLI tool and bindings for MIG device management.
+Supports all MIG devices and supports both 1:1 gpu instance compute instances setups and setups where compute instances share gpu instances.
 
-Usage:
+- [radT on Github](https://github.com/Resource-Aware-Data-Systems-RAD/migedit)
+- [radT on PyPI](https://pypi.org/project/migedit/)
+
+# Usage (CLI)
 
 `migedit -i 2 -p 1 2 3`
 to create three instances (1g.5gb, 2g.10gb, 3g.20gb) on GPU 2.
@@ -11,9 +15,31 @@ or
 `migedit -i 2 -p s2 s2`
 to create two shared memory instances (2c.7g.40gb, 2c.7g.40gb) on GPU 2.
 
+# Usage (Python bindings)
+
+`import migedit`
+to import migedit in your project.
+
+`migedit.get_mig_profiles()`
+to get a list of all available MIG instance configurations.
+
+`migedit.make_mig_devices(0, ["1g.10gb"])`
+to remove old MIG instances and create new ones.
 
 # Changelog:
-
+- 3.0: Support for non-A100 devices (H100, A30, etc.) by dynamically grabbing available profiles.
 - 2.0: Support for Shared Memory Mig Mode (with 7g.40gb instances)
 - 1.1: Various bugfixes
 - 1.0: Initial
+
+## Supported platforms
+
+- [x] Linux (Python 3.10 or higher)
+
+## Contributors
+
+- [Ties Robroek](https://github.com/sipondo)
+
+Thank You!
+
+Contributions are welcome. _(Please add yourself to the list)_
--- migedit.py
+++ migedit.py
@@ -1,36 +1,30 @@
 """MIG Editor. CLI/importable module to automatically delete and create MIG gpu/cpu instances."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 import argparse
 import os
+import re
 import time
 
 from subprocess import Popen, PIPE
 
-
-MIG_OPTIONS = {
-    "1": "1g.5gb",
-    "2": "2g.10gb",
-    "3": "3g.20gb",
-    "4": "4g.20gb",
-    "7": "7g.40gb",
-}
-SMIG_OPTIONS = {
-    "s1": "1c.7g.40gb",
-    "s2": "2c.7g.40gb",
-    "s3": "3c.7g.40gb",
-    "s4": "4c.7g.40gb",
-}
+RE_OPTIONS = re.compile("((\S*)g\.\S*)")
+NEWTAB = "\n\t"
 
 
-def execute_command(cmd, shell=False, vars={}):
+def _execute_command(cmd: str | list, shell: bool = False, vars: dict = {}) -> list:
+    """Executes a command
+
+    Args:
+        cmd (str | list): Command to run
+        shell (bool, optional): Whether to run with shell enabled. Defaults to False.
+        vars (dict, optional): Environment variables to pass on. Defaults to {}.
+
+    Returns:
+        list: Command output
     """
-    Executes a (non-run) command.
-    :param cmd -> Command to run.
 
-    :return result -> Text printed by the command.
-    """
     if isinstance(cmd, str):
         cmd = cmd.split()
 
@@ -50,8 +44,18 @@ def execute_command(cmd, shell=False, vars={}):
     return result
 
 
-def get_dcgmi_instance_id(device, gpu_instance):
-    for line in execute_command("dcgmi discovery -c"):
+def _get_dcgmi_instance_id(device: int, gpu_instance: int) -> str | None:
+    """Get entity ID of GPU instance
+
+    Args:
+        device (int): GPU device to search
+        gpu_instance (int): GPU instance to search for
+
+    Returns:
+        str | None: DCGMI instance id
+    """
+
+    for line in _execute_command("dcgmi discovery -c"):
         if "EntityID" not in line:
             continue
 
@@ -64,28 +68,75 @@ def get_dcgmi_instance_id(device, gpu_instance):
     return None
 
 
-def get_mig_ids(device):
+def _get_mig_ids(device: int) -> set:
+    """Get NVIDIA SMI ids for all MIG compute instances on the device
+
+    Args:
+        device (int): GPU device to get instances of
+
+    Returns:
+        set: Set of id strings of all instances
+    """
+
     gpu = None
     ids = set()
-    for line in execute_command("nvidia-smi -L"):
+    for line in _execute_command("nvidia-smi -L"):
         if "UUID: GPU" in line:
             gpu = int(line.split("GPU")[1].split(":")[0].strip())
             continue
         elif "UUID: MIG" in line:
-            if gpu == int(device):
+            if gpu == device:
                 ids.add(line.split("UUID:")[1].split(")")[0].strip())
 
     return ids
 
 
-def make_mig_devices(gpu: str, profiles: list, verbose=False) -> tuple:
+def _list_transpose(input_list: list) -> list:
+    """Transpose a list containing iterables
+
+    Args:
+        input_list (list): List of iterables to transpose
+
+    Returns:
+        list: Transposed list
+    """
+    return [list(i) for i in zip(*input_list)]
+
+
+def make_mig_devices(
+    gpu: int, profiles: list, available_mig: list = [], available_smig: list = []
+) -> list:
+    """Remove any old MIG instances and create MIG instances on gpu following profiles
+
+    Args:
+        gpu (int): GPU instance to create on
+        profiles (list): List of profiles to use
+        available_mig (list, optional): Available MIG profiles. Automatically retrieves profiles if empty
+        available_smig (list, optional): Available shared MIG profiles. Automatically retrieves profiles if empty
+
+    Raises:
+        ValueError: Command failed
+
+    Returns:
+        list: Allocated MIG instances
+
+    """
+
+    # Retrieve missing profile availability data
+    if (not available_mig) or (not available_smig):
+        options_mig, options_smig = get_mig_profiles()
+
+        if not available_mig:
+            available_mig = options_mig
+        if not available_smig:
+            available_smig = options_smig
 
     # Remove old instances
-    result = "".join(execute_command(f"sudo nvidia-smi mig -dci")).lower()
+    result = "".join(_execute_command(f"sudo nvidia-smi mig -dci")).lower()
     if "failed" in result or "unable" in result:
         raise ValueError(result)
 
-    result = "".join(execute_command(f"sudo nvidia-smi mig -dgi")).lower()
+    result = "".join(_execute_command(f"sudo nvidia-smi mig -dgi")).lower()
     if "failed" in result or "unable" in result:
         raise ValueError(result)
 
@@ -101,17 +152,19 @@ def make_mig_devices(gpu: str, profiles: list, verbose=False) -> tuple:
     for index, profile in enumerate(profiles):
         instance = profile.lower().strip()
 
-        if instance in MIG_OPTIONS:
-            instance = MIG_OPTIONS[instance]
-        elif instance in SMIG_OPTIONS:
-            instance = SMIG_OPTIONS[instance]
+        if instance in available_mig:
+            instance = available_mig[instance]
+        elif instance in available_smig:
+            instance = available_smig[instance]
 
         # Normal MIG
-        if instance in MIG_OPTIONS.values():
-            other_mig_ids = get_mig_ids(gpu)
+        if instance in available_mig.values():
+            other_mig_ids = _get_mig_ids(gpu)
 
             result = "".join(
-                execute_command(f"sudo nvidia-smi mig -i {int(gpu)} -cgi {instance} -C")
+                _execute_command(
+                    f"sudo nvidia-smi mig -i {int(gpu)} -cgi {instance} -C"
+                )
             ).lower()
 
             gpu_instance = result.split("gpu instance id")[1].split("on gpu")[0].strip()
@@ -119,33 +172,35 @@ def make_mig_devices(gpu: str, profiles: list, verbose=False) -> tuple:
             if "failed" in result or "unable" in result:
                 raise ValueError(result)
 
-            mig_id = get_mig_ids(gpu) - other_mig_ids
+            mig_id = _get_mig_ids(gpu) - other_mig_ids
 
             devicetemp.append((index, gpu, instance, gpu_instance, mig_id))
 
-        elif instance in SMIG_OPTIONS.values():
+        elif instance in available_smig.values():
             # Shared MIG, TODO: make more flexible
             if gpu not in shared_mig_gpui:
                 result = "".join(
-                    execute_command(f"sudo nvidia-smi mig -i {gpu} -cgi 7g.40gb")
+                    _execute_command(
+                        f"sudo nvidia-smi mig -i {gpu} -cgi {list(available_mig.values())[-1]}"
+                    )
                 ).lower()
                 instance_id = result.split("gpu instance id")[1].split("on")[0].strip()
                 shared_mig_gpui[gpu] = instance_id
 
-            other_mig_ids = get_mig_ids(gpu)
+            other_mig_ids = _get_mig_ids(gpu)
 
             gpu_instance = shared_mig_gpui[gpu]
 
             result = "".join(
-                execute_command(
-                    f"sudo nvidia-smi mig -gi {gpu_instance} -cci {list(SMIG_OPTIONS.values()).index(instance)}"
+                _execute_command(
+                    f"sudo nvidia-smi mig -gi {gpu_instance} -cci {list(available_smig.values()).index(instance)}"
                 )
             ).lower()
 
             if "failed" in result or "unable" in result:
                 raise ValueError(result)
 
-            mig_id = get_mig_ids(gpu) - other_mig_ids
+            mig_id = _get_mig_ids(gpu) - other_mig_ids
 
             devicetemp.append((index, gpu, instance, gpu_instance, mig_id))
 
@@ -155,9 +210,8 @@ def make_mig_devices(gpu: str, profiles: list, verbose=False) -> tuple:
 
     results = []
 
-    for (index, device, instance, gpu_instance, mig_id) in devicetemp:
-
-        while (entity_id := get_dcgmi_instance_id(device, gpu_instance)) is None:
+    for index, device, instance, gpu_instance, mig_id in devicetemp:
+        while (entity_id := _get_dcgmi_instance_id(device, gpu_instance)) is None:
             time.sleep(
                 0.5
             )  # Wait for the rather slow DCGMI discovery to yield a GPU entity id
@@ -170,14 +224,65 @@ def make_mig_devices(gpu: str, profiles: list, verbose=False) -> tuple:
     return results
 
 
-def cli():
+def get_mig_profiles() -> tuple[dict, dict]:
+    """Retrieve all available mig profiles
+
+    Returns:
+        tuple[dict, dict]: All available MIG and shared MIG profiles
+    """
+
+    # Get profile lists
+    list_mig, scales = _list_transpose(
+        [
+            x[0]
+            for line in _execute_command("nvidia-smi mig -lgip")
+            if (x := RE_OPTIONS.findall(line))
+        ]
+    )
+    list_smig = [
+        f"{compute}c.{list_mig[-1]}" for compute in sorted(list(set(scales)))[:-1]
+    ]
+
+    # Convert to dicts
+    available_mig, available_smig = {}, {}
+    for profile in list_mig:
+        original_prefix = profile.split("g")[0]
+        prefix = original_prefix
+
+        i = 0
+        while prefix in available_mig:
+            i += 1
+            prefix = f"{original_prefix}.{i}"
+
+        available_mig[prefix] = profile
+
+    for profile in list_smig:
+        original_prefix = "s" + profile.split("c")[0]
+        prefix = original_prefix
+
+        i = 0
+        while prefix in available_mig:
+            i += 1
+            prefix = f"{original_prefix}.{i}"
+
+        available_smig[prefix] = profile
+
+    return available_mig, available_smig
+
+
+def _cli():
+    available_mig, available_smig = get_mig_profiles()
+
     parser = argparse.ArgumentParser(
         description="MIG Editor. CLI/importable module to automatically delete and create MIG gpu/cpu instances.",
+        formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
         "-i",
         "--instance",
+        type=int,
         metavar="GPU",
+        default=0,
         help="ID instance number of the GPU to manage MIG on.",
     )
     parser.add_argument(
@@ -185,14 +290,14 @@ def cli():
         "--profiles",
         metavar="PROFILES",
         nargs="+",
-        help="""Space seperated list of MIG profiles to use. Options: "1" or "1g.5gb", "2" or "2g.10gb", "3" or "3g.20gb", "4" or "4g.20gb", "7" or "7g.40gb", "s1" or "1c.7g.40gb", "s2" or "2c.7g.40gb", "s3" or "3c.7g.40gb", "s4" or "4c.7g.40gb".""",
+        help=f"""Space seperated list of MIG profiles to use.\nOptions:\n\t{f"{NEWTAB}".join([f'{k: <3} or {v}' for k,v in (available_mig | available_smig).items()])}""",
         required=True,
     )
 
     args = parser.parse_args()
 
-    make_mig_devices(args.instance, args.profiles)
+    make_mig_devices(args.instance, args.profiles, available_mig, available_smig)
 
 
 if __name__ == "__main__":
-    cli()
+    _cli()
